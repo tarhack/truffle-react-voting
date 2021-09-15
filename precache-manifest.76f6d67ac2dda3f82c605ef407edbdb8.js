2ad57f228e34953459c4d2d834c60dab1b398edd
tarhack <tarhack@gmail.com>
2021-09-15 19:10:27 +0200

    Updates

---
+++ precache-manifest.76f6d67ac2dda3f82c605ef407edbdb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb9023635585267b5fc22b693b372de",
+    "url": "/truffle-react-voting/index.html"
+  },
+  {
+    "revision": "f2d683b3a27d7b5aafc4",
+    "url": "/truffle-react-voting/static/css/2.25729603.chunk.css"
+  },
+  {
+    "revision": "c4f9ea6a7c28a1b04c29",
+    "url": "/truffle-react-voting/static/css/main.017233eb.chunk.css"
+  },
+  {
+    "revision": "f2d683b3a27d7b5aafc4",
+    "url": "/truffle-react-voting/static/js/2.e84744fa.chunk.js"
+  },
+  {
+    "revision": "c4f9ea6a7c28a1b04c29",
+    "url": "/truffle-react-voting/static/js/main.cc424a82.chunk.js"
+  },
+  {
+    "revision": "2c8cd9a662cde6f65e1f",
+    "url": "/truffle-react-voting/static/js/runtime-main.ec9cffe7.js"
+  }
+]);
